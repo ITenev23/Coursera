ff174db8553d4eea11c515f008114236a72ed504
almanaha <almanah.soft@gmail.com>
2020-07-21 20:18:44 +0300

    maven setup

---
+++ src/main/java/com/coursera/itenev/CourseraApplication.java
@@ -0,0 +1,12 @@
+package com.coursera.itenev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseraApplication.class, args);
+    }
+}
